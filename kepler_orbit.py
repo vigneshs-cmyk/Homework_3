3628d35e8fa96da5059194ba6e371db39f88d416
vigneshs-cmyk <vigneshs@umich.edu>
2026-02-24 19:07:30 -0500

    Completed the energy calculations and plotted the results

--- kepler_orbit.py
+++ kepler_orbit.py
@@ -20,7 +20,7 @@ ry0 = 0
 vx0 = 0
 vy0 = 0.3/(np.sqrt(rx0))
 
-dt =  [0.125, 0.100, 0.075]
+dt =  [0.125, 0.100, 0.075, 0.01]
 Nt = 10000
 
 def accecleration(rx, ry):
@@ -57,7 +57,14 @@ def velocity_verlet(rx, ry, vx, vy, dt):
         vx[i+1] = vx[i] + 0.5 * (ax + ax_next) * dt
         vy[i+1] = vy[i] + 0.5 * (ay + ay_next) * dt
 
-        
+def total_energy(rx, ry, vx, vy, Nt):
+    E_total = np.zeros(Nt)
+    for i in range(Nt):
+        r = np.sqrt(rx[i]**2 + ry[i]**2)
+        kinetic_energy = 0.5 * (vx[i]**2 + vy[i]**2)
+        potential_energy = -1 / r
+        E_total[i] = kinetic_energy + potential_energy
+    return E_total    
 
 #Implemeting the numerical solution using the 2nd order Runge-Kutta method
 rx_RK = np.zeros(Nt)
@@ -68,6 +75,10 @@ vx_RK = np.zeros(Nt)
 vy_RK = np.zeros(Nt)
 vx_RK[0] = vx0
 vy_RK[0] = vy0
+
+
+
+
 # Implemeting the numerical solution using the velocity verlet method
 rx_vv = np.zeros(Nt)
 ry_vv = np.zeros(Nt)
@@ -81,6 +92,7 @@ vy_vv[0] = vy0
 for i in range(len(dt)):
     velocity_verlet(rx_vv, ry_vv, vx_vv, vy_vv, dt[i])
     RK2(rx_RK, ry_RK, vx_RK, vy_RK, dt[i])
+
     plt.plot(rx0, ry0, 'bo', label='Initial Position', markersize=8)  # plot the initial position
 
     #plotting the RK2 numerical solution
@@ -89,9 +101,29 @@ for i in range(len(dt)):
     plt.plot(rx_vv, ry_vv, color='blue', label='Velocity Verlet Solution', linewidth=2)  # plot the numerical solution
     #Plotting the annalytic solution
     plt.plot(x, y, 'k--', label='Analytic Solution')  # plot the analytic solution
-    plt.legend()
+    plt.legend(loc='upper left')
     plt.xlabel('x')
     plt.ylabel('y') 
+
+    plt.setp(plt.gca().get_xticklabels(), fontsize=12)
+    plt.setp(plt.gca().get_yticklabels(), fontsize=12)
     plt.title('Kepler Orbit (analytic solution vs Numerical solution) dt = ' + str(dt[i]))
     plt.savefig('kepler_orbit_dt' + str(dt[i]) + '.png')
+    plt.show()
+
+    #Energy RK2
+    E_RK = total_energy(rx_RK, ry_RK, vx_RK, vy_RK, Nt)
+    #Energy velocity verlet
+    E_vv = total_energy(rx_vv, ry_vv, vx_vv, vy_vv, Nt)
+
+    #plottting the total energy vs time for both methods
+    plt.plot(np.arange(Nt) * dt[i], E_RK, color='red', label='RK2 Total Energy', linewidth=2)
+    plt.plot(np.arange(Nt) * dt[i], E_vv, color='blue', label='Velocity Verlet Total Energy', linewidth=2)
+    plt.xlabel('Time') 
+    plt.ylabel('Total Energy')
+    plt.setp(plt.gca().get_xticklabels(), fontsize=12)
+    plt.setp(plt.gca().get_yticklabels(), fontsize=12)
+    plt.title('Total Energy vs Time for Kepler Orbit dt = ' + str(dt[i]))
+    plt.legend(loc='upper left')
+    plt.savefig('kepler_energy_dt' + str(dt[i]) + '.png')
     plt.show()
