86b8f0b1e9a5622223d7ad2a92f5ebc26b707155
vigneshs-cmyk <vigneshs@umich.edu>
2026-02-25 20:38:45 -0500

    simplified Rk method implementation

--- kepler_orbit.py
+++ kepler_orbit.py
@@ -13,6 +13,18 @@ r = p / (1 + e * np.cos(theta))
 x = r * np.cos(theta)
 y = r * np.sin(theta)
 
+plt.plot(x, y, 'k--', label='Analytic Solution')  # plot the analytic solution
+
+plt.legend(loc='upper right', fontsize=14)
+plt.xlabel('x', fontsize=15)
+plt.ylabel('y', fontsize=15) 
+
+plt.setp(plt.gca().get_xticklabels(), fontsize=15)
+plt.setp(plt.gca().get_yticklabels(), fontsize=15)
+plt.title('Kepler Orbit (analytic solution)', fontsize=15)
+plt.savefig('kepler_orbit_analytic.png')
+plt.show()
+
 
 #initial setup for the numerical solution
 rx0 = 10
@@ -20,7 +32,7 @@ ry0 = 0
 vx0 = 0
 vy0 = 0.3/(np.sqrt(rx0))
 
-dt =  [0.125, 0.100, 0.075, 0.01]
+dt =  [0.125, 0.100, 0.075]
 Nt = 10000
 
 def accecleration(rx, ry):
@@ -31,21 +43,17 @@ def accecleration(rx, ry):
 
 def RK2(rx, ry, vx, vy, dt):
     for i in range(Nt - 1):
-        ax1, ay1 = accecleration(rx[i], ry[i])          # k1 for velocity
-        k1x, k1y = vx[i], vy[i]
-        k1vx, k1vy = ax1, ay1
-
-        rx_mid = rx[i] + 0.5 * dt * k1x
-        ry_mid = ry[i] + 0.5 * dt * k1y
-        vx_mid = vx[i] + 0.5 * dt * k1vx
-        vy_mid = vy[i] + 0.5 * dt * k1vy
-
-        ax2, ay2 = accecleration(rx_mid, ry_mid)        # k2 for velocity
-
+        ax, ay = accecleration(rx[i], ry[i])
+        rx_mid = rx[i] + 0.5 * dt * vx[i]
+        ry_mid = ry[i] + 0.5 * dt * vy[i]
+        vx_mid = vx[i] + 0.5 * dt * ax
+        vy_mid = vy[i] + 0.5 * dt * ay
+        
+        ax_mid, ay_mid = accecleration(rx_mid, ry_mid)
         rx[i+1] = rx[i] + dt * vx_mid
         ry[i+1] = ry[i] + dt * vy_mid
-        vx[i+1] = vx[i] + dt * ax2
-        vy[i+1] = vy[i] + dt * ay2
+        vx[i+1] = vx[i] + dt * ax_mid
+        vy[i+1] = vy[i] + dt * ay_mid
 
 def velocity_verlet(rx, ry, vx, vy, dt):
     for i in range(Nt - 1):
@@ -66,30 +74,29 @@ def total_energy(rx, ry, vx, vy, Nt):
         E_total[i] = kinetic_energy + potential_energy
     return E_total    
 
-#Implemeting the numerical solution using the 2nd order Runge-Kutta method
-rx_RK = np.zeros(Nt)
-ry_RK = np.zeros(Nt)
-rx_RK[0] = rx0
-ry_RK[0] = ry0
-vx_RK = np.zeros(Nt)
-vy_RK = np.zeros(Nt)
-vx_RK[0] = vx0
-vy_RK[0] = vy0
 
-
-
-
-# Implemeting the numerical solution using the velocity verlet method
-rx_vv = np.zeros(Nt)
-ry_vv = np.zeros(Nt)
-rx_vv[0] = rx0
-ry_vv[0] = ry0 
-vx_vv = np.zeros(Nt)
-vy_vv = np.zeros(Nt)
-vx_vv[0] = vx0
-vy_vv[0] = vy0
 
 for i in range(len(dt)):
+    #Implemeting the numerical solution using the 2nd order Runge-Kutta method
+    rx_RK = np.zeros(Nt)
+    ry_RK = np.zeros(Nt)
+    rx_RK[0] = rx0
+    ry_RK[0] = ry0
+    vx_RK = np.zeros(Nt)
+    vy_RK = np.zeros(Nt)
+    vx_RK[0] = vx0
+    vy_RK[0] = vy0
+
+    # Implemeting the numerical solution using the velocity verlet method
+    rx_vv = np.zeros(Nt)
+    ry_vv = np.zeros(Nt)
+    rx_vv[0] = rx0
+    ry_vv[0] = ry0 
+    vx_vv = np.zeros(Nt)
+    vy_vv = np.zeros(Nt)
+    vx_vv[0] = vx0
+    vy_vv[0] = vy0
+    
     velocity_verlet(rx_vv, ry_vv, vx_vv, vy_vv, dt[i])
     RK2(rx_RK, ry_RK, vx_RK, vy_RK, dt[i])
 
@@ -101,14 +108,15 @@ for i in range(len(dt)):
     plt.plot(rx_vv, ry_vv, color='blue', label='Velocity Verlet Solution', linewidth=2)  # plot the numerical solution
     #Plotting the annalytic solution
     plt.plot(x, y, 'k--', label='Analytic Solution')  # plot the analytic solution
-    plt.legend(loc='upper left')
-    plt.xlabel('x')
-    plt.ylabel('y') 
+    plt.legend(loc='upper right', fontsize=14)
+    plt.xlabel('x', fontsize=15)
+    plt.ylabel('y', fontsize=15) 
 
-    plt.setp(plt.gca().get_xticklabels(), fontsize=12)
-    plt.setp(plt.gca().get_yticklabels(), fontsize=12)
+    plt.setp(plt.gca().get_xticklabels(), fontsize=15)
+    plt.setp(plt.gca().get_yticklabels(), fontsize=15)
+    plt.title('Kepler Orbit (analytic solution)', fontsize=15)
     plt.title('Kepler Orbit (analytic solution vs Numerical solution) dt = ' + str(dt[i]))
-    plt.savefig('kepler_orbit_dt' + str(dt[i]) + '.png')
+    #plt.savefig('kepler_orbit_dt' + str(dt[i]) + '.png')
     plt.show()
 
     #Energy RK2
@@ -124,6 +132,6 @@ for i in range(len(dt)):
     plt.setp(plt.gca().get_xticklabels(), fontsize=12)
     plt.setp(plt.gca().get_yticklabels(), fontsize=12)
     plt.title('Total Energy vs Time for Kepler Orbit dt = ' + str(dt[i]))
-    plt.legend(loc='upper left')
+    plt.legend(loc='upper right')
     plt.savefig('kepler_energy_dt' + str(dt[i]) + '.png')
     plt.show()
